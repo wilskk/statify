5e1a8d988e6e810e4637759333a184739a56f729
HIMAWAN WAHID IKHWANSYAH <222112094@stis.ac.id>
2025-03-20 17:06:15 +0700

    Remove dist folder from repository

--- dist/server/routes/savRoutes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const savController_1 = require("../controllers/savController");
-const router = (0, express_1.Router)();
-router.post('/upload', savController_1.uploadSavFile);
-router.post('/create', savController_1.createSavFile);
-exports.default = router;
-//# sourceMappingURL=savRoutes.js.map
