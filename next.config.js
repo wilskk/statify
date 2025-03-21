2cec91a46e2f5f1a66960635120e41627ac0aadf
HIMAWAN WAHID IKHWANSYAH <222112094@stis.ac.id>
2025-03-21 16:03:51 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,26 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    poweredByHeader: false,
+    async headers() {
+        return [
+            {
+                source: '/:path*',
+                headers: [
+                    {
+                        key: 'X-Middleware-Preflight',
+                        value: '1',
+                    },
+                ],
+            },
+        ];
+    },
+
+    // If you're using the app router, you might need these:
+    experimental: {
+        // ...other experimental options
+        skipTrailingSlashRedirect: true,
+        skipMiddlewareUrlNormalize: true,
+    }
+}
 
 module.exports = nextConfig
