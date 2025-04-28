2ecf48566449921aae8780a6ff9a577b4829c971
Guswana <222112082@stis.ac.id>
2025-04-28 14:22:29 +0700

    penyusunan library rust

--- src/wasm/src/lib.rs
+++ src/wasm/src/lib.rs
@@ -1,8 +1,19 @@
+// Time Series Analysis
 pub mod time_series;
+
+// Smoothing
 pub use time_series::smoothing::smoothing::Smoothing;
+
+// Decomposition
 pub use time_series::decomposition::decomposition::Decomposition;
+
+// Difference
 pub use time_series::difference::difference::*;
+
+// Autocorrelation
 pub use time_series::autocorrelation::autocorrelation::Autocorrelation;
+
+// Unit Root Test
 pub use time_series::unit_root_test::calculate_critical_values::*;
 pub use time_series::unit_root_test::calculate_pvalue::*;
 pub use time_series::unit_root_test::mackinnon_critical_values::MacKinnonCriticalValues;
@@ -12,6 +23,7 @@ pub use time_series::unit_root_test::read_pvalue::*;
 pub use time_series::unit_root_test::dickey_fuller::dickey_fuller::DickeyFuller;
 pub use time_series::unit_root_test::augmented_dickey_fuller::augmented_dickey_fuller::AugmentedDickeyFuller;
 
+// ARIMA
 pub use time_series::arima::arima::Arima;
 pub use time_series::arima::est_coef_process::autocov::*;
 pub use time_series::arima::est_coef_process::durb_lev_alg::durb_lev_alg;
@@ -19,7 +31,10 @@ pub use time_series::arima::est_coef_process::innov_alg::innov_alg;
 pub use time_series::arima::est_coef_process::css::css;
 pub use time_series::arima::est_coef_process::est_coef::est_coef;
 
+// Regression Analysis
 pub mod regression;
+
+// Regression Computing
 pub use regression::simple_linear_regression::simple_linear_regression::SimpleLinearRegression;
 pub use regression::no_intercept_linear_regression::no_intercept_linear_regression::NoInterceptLinearRegression;
 pub use regression::simple_exponential_regression::simple_exponential_regression::SimpleExponentialRegression;
