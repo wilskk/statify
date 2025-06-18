b640447f9a3e341e51bf756ff26377d31833286d
HIMAWAN WAHID IKHWANSYAH <222112094@stis.ac.id>
2025-06-18 12:35:33 +0700

    update aggregate

--- frontend/components/Modals/Data/Aggregate/hooks/useAggregateData.ts
+++ frontend/components/Modals/Data/Aggregate/hooks/useAggregateData.ts
@@ -9,7 +9,7 @@ import {
     mapUIFunctionToCalculationFunction,
     getFunctionDisplay,
     calculateAggregateValue
-} from "@/components/Modals/Data/Aggregate/utils";
+} from "@/components/Modals/Data/Aggregate/aggregateUtils";
 
 export const useAggregateData = () => {
     const { variables } = useVariableStore();
